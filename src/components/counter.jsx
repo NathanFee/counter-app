8003178cfebe8e0320910a50203aa0eb97c72381
NathanFee <Nathan_Fee@yahoo.com>
2019-06-19 12:16:39 -0400

    Adds event handler to increment button

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,16 +2,46 @@ import React, { Component, Fragment } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0
+    count: 0,
+    tags: ["tag1", "tag3", "tag3"]
   };
+
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
   render() {
     return (
       <Fragment>
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
       </Fragment>
     );
   }
+
+  //   renderTags() {
+  //     if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+
+  //     return (
+  //       <ul>
+  //         {this.state.tags.map(tag => (
+  //           <li key={tag}>{tag}</li>
+  //         ))}
+  //       </ul>
+  //     );
+  //   }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
   formatCount() {
     const { count } = this.state;
     return count === 0 ? "Zero" : count;
